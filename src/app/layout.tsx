f0c03cd17608d38b011df1486201ca351a4005a4
iair kaplun <iairkap@gmail.com>
2024-07-03 17:42:35 -0300

    error

---
+++ src/app/layout.tsx
@@ -0,0 +1,17 @@
+
+
+import "./globals.css";
+import GlobalContextProvider from "./userContext"; // Ajusta la ruta según sea necesario
+import Script from "next/script";
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <GlobalContextProvider>
+      <html lang="es">
+        <body>
+          {children}
+        </body>
+      </html>
+    </GlobalContextProvider>
+  );
+}
