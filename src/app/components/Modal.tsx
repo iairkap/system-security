c3fb05852092ee45d707c994c8be8830664cad45
iair kaplun <iairkap@gmail.com>
2024-07-04 11:56:12 -0300

    addedAnimations

--- src/app/components/Modal.tsx
+++ src/app/components/Modal.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactModal from 'react-modal';
+import styles from "../style/modalLayout.module.css"
 
 
 function Modal({ children, isOpen, onRequestClose }: { children: React.ReactNode, isOpen: boolean, onRequestClose: () => void }) {
@@ -7,33 +8,8 @@ function Modal({ children, isOpen, onRequestClose }: { children: React.ReactNode
         <ReactModal
             isOpen={isOpen}
             onRequestClose={onRequestClose}
-
-            style={{
-                overlay: {
-                    backgroundColor: 'rgba(0, 0, 0, 0.5)',
-                    zIndex: 1000,
-                    backdropFilter: 'blur(5px)',
-                },
-                content: {
-                    top: '50%',
-                    left: '50%',
-                    right: 'auto',
-                    bottom: 'auto',
-                    marginRight: '-50%',
-                    transform: 'translate(-50%, -50%)',
-                    border: 'none',
-                    padding: '2rem',
-                    borderRadius: '10px',
-                    width: '50%',
-                    height: '50%',
-                    display: 'flex',
-                    justifyContent: 'center',
-                    alignItems: 'center',
-                    background: '#121212',
-                    color: "white"
-
-                },
-            }}
+            className={styles.content}
+            overlayClassName={styles.overlay}
         >
             {children}
         </ReactModal>
